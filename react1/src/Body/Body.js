1d30c5bd03a174fcdd4e0ab880c3aff7bc90c7c7
lichendd <47678173+lichendd@users.noreply.github.com>
2022-05-29 19:16:40 -0400

    working on add the scroll feature

--- react1/src/Body/Body.js
+++ react1/src/Body/Body.js
@@ -1,21 +1,22 @@
-import Questions from "../questions"
-import Question from "./Question"
+import Questions from "../questions";
+import Question from "./Question";
 import { useNavigate } from "react-router-dom";
-import "./BodyStyle.css"
+import "./BodyStyle.css";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 function Body() {
-    return (
-      <>
-        <h1>Student Information Survey</h1>
-        <h2>Please fillin this survey to help us better understand you</h2>
-        {Questions.map((item) => (
-          <Question key={item.id} item={item} />
-        ))}
-        <button type="button" class="btn btn-light">
-          Submit
-        </button>
-      </>
-    );
+  return (
+    <div className="container-fluid">
+      <h1>Student Information Survey</h1>
+      <h2>Please fillin this survey to help us better understand you</h2>
+      {Questions.map((item) => (
+        <Question key={item.id} item={item} />
+      ))}
+      <button type="button" class="btn btn-light">
+        Submit
+      </button>
+    </div>
+  );
 }
 
 export default Body;
