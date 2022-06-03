415709e95035d9554e6cf0d9883fbed5202e6643
lichendd <47678173+lichendd@users.noreply.github.com>
2022-06-02 20:14:51 -0400

    changed fonts

--- react1/src/Body/Question.js
+++ react1/src/Body/Question.js
@@ -1,29 +1,39 @@
 // A Question component displaies one question in questions.json
 import "./QuestionStyle.css"
 import TextField from "./TextField.tsx";
+import { Text, StyleSheet } from "react-native";
 
 function Question({key, item}) {
     const hasChoice = item.hasChoice;
-    return(
-        <div className="question">
-            {!hasChoice ? (
-                <div>
-                    <div className="question">Question {item.id} {item.question}</div>
-                    <TextField />
-                </div>
-            ):(
-                <div>
-                    <div className="question">Question {item.id} {item.question}</div>
-                    {item.choices.map((choice) => (
-                        <div className="topping">
-                            <input type="checkbox" id="topping" name="topping" 
-                            value="Choice" />
-                             {choice}
-                        </div>
-                    ))}
-                </div>
-            )}
-        </div>
+    return (
+      <div className="question">
+        {!hasChoice ? (
+          <div>
+            <div className="question">
+              <Text style={{ fontWeight: "bold" }}>Question {item.id}</Text>{" "}
+              {item.question}
+            </div>
+            <TextField />
+          </div>
+        ) : (
+          <div>
+            <div className="question">
+              Question {item.id} {item.question}
+            </div>
+            {item.choices.map((choice) => (
+              <div className="topping">
+                <input
+                  type="checkbox"
+                  id="topping"
+                  name="topping"
+                  value="Choice"
+                />
+                {choice}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     );
 }
 
