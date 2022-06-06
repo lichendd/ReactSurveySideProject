f273677c356a5480de6531c40b5bebb8117792f2
lichendd <47678173+lichendd@users.noreply.github.com>
2022-06-05 20:49:38 -0400

    text placeholder

--- react1/src/Body/Question.js
+++ react1/src/Body/Question.js
@@ -1,42 +1,47 @@
 // A Question component displaies one question in questions.json
-import "./QuestionStyle.css"
+import "./QuestionStyle.css";
 import TextField from "./TextField.tsx";
 import { Text, StyleSheet } from "react-native";
 
-function Question({key, item}) {
-    const hasChoice = item.hasChoice;
-    return (
-      <div className="question">
-        {!hasChoice ? (
-          <div>
-            <div className="question">
-              <Text style={{ fontWeight: "bold" }}>Question {item.id}</Text>{" "}
-              {item.question}
-            </div>
-            <TextField />
+function Question({ key, item }) {
+  const hasChoice = item.hasChoice;
+  return (
+    <div className="question">
+      {!hasChoice ? (
+        <div>
+          <div className="question">
+            <Text style={{ fontWeight: "bold" }}>Question {item.id}</Text>{" "}
+            {item.question}
           </div>
-        ) : (
-          <div>
-            <div className="question">
-              <Text style={{ fontWeight: "bold" }}>Question {item.id}</Text>{" "}
-               {item.question}
-            </div>
-            {item.choices.map((choice) => (
-              <div className="topping">
-                <input
-                  type="checkbox"
-                  id="topping"
-                  name="topping"
-                  value="Choice"
-                />
-                <Text>{"  "}</Text>
-                {choice}
-              </div>
-            ))}
+          <input
+            type="text"
+            class="main-input main-name"
+            placeholder={"This is placeholder"}
+          />
+          {/* <TextField placeholder={"This is placeholder"} /> */}
+        </div>
+      ) : (
+        <div>
+          <div className="question">
+            <Text style={{ fontWeight: "bold" }}>Question {item.id}</Text>{" "}
+            {item.question}
           </div>
-        )}
-      </div>
-    );
+          {item.choices.map((choice) => (
+            <div className="topping">
+              <input
+                type="checkbox"
+                id="topping"
+                name="topping"
+                value="Choice"
+              />
+              <Text>{"  "}</Text>
+              {choice}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Question;
